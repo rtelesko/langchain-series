c99194fe3b21fee1d1a9f558026e70d1985f807e
Rainer Telesko <rainer.telesko@gmail.com>
2024-03-27 18:49:00 +0100

    Updates: Layout improvements Uploading multiple PDF files Replacing "grade" with "level" (of difficulty)

--- 001_ed_tech_quiz/app.py
+++ 001_ed_tech_quiz/app.py
@@ -1,4 +1,6 @@
 import json
+from PIL import Image
+
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
@@ -8,8 +10,10 @@ import streamlit as st
 import traceback
 import pandas as pd
 from langchain.callbacks import get_openai_callback
+
 from utils import parse_file, get_table_data, RESPONSE_JSON
 
+# Load OpenAI API_KEY
 load_dotenv()
 
 # This is an LLMChain to create 10-20 multiple choice questions from a given piece of text.
@@ -18,7 +22,7 @@ llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0, max_tokens=-1)
 template = """
 Text: {text}
 You are an expert MCQ maker. Given the above text, it is your job to\
-create a quiz of {number} multiple choice questions for grade {grade} students in {tone} tone.
+create a quiz of {number} multiple choice questions with a {level} level of difficulty in {tone} tone.
 Make sure that questions are not repeated and check all the questions to be conforming to the text as well.
 Make sure to format your response like the RESPONSE_JSON below and use it as a guide.\
 Ensure to make the {number} MCQs.
@@ -26,7 +30,7 @@ Ensure to make the {number} MCQs.
 {response_json}
 """
 quiz_generation_prompt = PromptTemplate(
-    input_variables=["text", "number", "grade", "tone", "response_json"],
+    input_variables=["text", "number", "level", "tone", "response_json"],
     template=template,
 )
 quiz_chain = LLMChain(
@@ -35,7 +39,7 @@ quiz_chain = LLMChain(
 
 # This is an LLMChain to evaluate the multiple choice questions created by the above chain
 llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0)
-template = """You are an expert english grammarian and writer. Given a multiple choice quiz for {grade} grade students.\
+template = """You are an expert english grammarian and writer. Given a multiple choice quiz with a {level} level of difficulty.\
 You need to evaluate complexity of the questions and give a complete analysis of the quiz if the students 
 will be able to understand the questions and answer them. Only use at max 50 words for complexity analysis.
 If quiz is not at par with the cognitive and analytical abilities of the students,\
@@ -45,7 +49,7 @@ Quiz MCQs:
 Critique from an expert english writer of the above quiz:"""
 
 quiz_evaluation_prompt = PromptTemplate(
-    input_variables=["grade", "quiz"], template=template
+    input_variables=["level", "quiz"], template=template
 )
 review_chain = LLMChain(
     llm=llm, prompt=quiz_evaluation_prompt, output_key="review", verbose=True
@@ -54,31 +58,53 @@ review_chain = LLMChain(
 # This is the overall chain where we run these two chains in sequence.
 generate_evaluate_chain = SequentialChain(
     chains=[quiz_chain, review_chain],
-    input_variables=["text", "number", "grade", "tone", "response_json"],
+    input_variables=["text", "number", "level", "tone", "response_json"],
     # Here we return multiple variables
     output_variables=["quiz", "review"],
     verbose=True,
 )
 
-st.title("🦜⛓️ Langchain-Series: 001-Quiz Generation for Educational Content")
+st.markdown("<h1 style='text-align: center; color: red;'>Quiz Generation for Educational Content</h1>",
+            unsafe_allow_html=True)
+st.balloons()
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.write(' ')
+with col2:
+    image = Image.open('img/quiz.jpg')
+    st.image(image)
+with col3:
+    st.write(' ')
 
 # Create a form using st.form
 with st.form("user_inputs"):
     # File upload
-    uploaded_file = st.file_uploader("Upload a pdf or text file")
+    uploaded_files = st.file_uploader("Upload one or more pdf or text files", accept_multiple_files=True)
 
     # Input fields
-    mcq_count = st.number_input("No of MCQs", min_value=3, max_value=20)
-    grade = st.number_input("Insert Grade", min_value=1, max_value=10)
-    tone = st.text_input("Insert Quiz tone", max_chars=100, placeholder="simple")
-
+    mcq_count = st.number_input("Number of MCQ", min_value=3, max_value=20)
+    level = st.radio(label="Select applicable level of difficulty", options=["easy", "medium", "complex"])
+    tone = st.radio(label="Select applicable tone for the quiz",
+                    options=["formal", "informal", "friendly", "professional"])
     button = st.form_submit_button("Create quiz")
 
 # Check if the button is clicked and all fields have inputs
-if button and uploaded_file is not None and mcq_count and grade and tone:
+if button and uploaded_files is not None and mcq_count and level and tone:
+
+    # Create a list to store the uploaded file names
+    file_list = []
+    st.write("Uploaded Files:")
+    for uploaded_file in uploaded_files:
+        if uploaded_file is not None:
+            # Append the name of the uploaded file to the file_list
+            file_list.append(uploaded_file)
+            st.markdown("- " + uploaded_file.name)
+
     with st.spinner("Loading..."):
         try:
-            text = parse_file(uploaded_file)
+            text = ""
+            for uploaded_file in uploaded_files:
+                text += parse_file(uploaded_file)
 
             # count tokens and cost of api call
             with get_openai_callback() as cb:
@@ -86,7 +112,7 @@ if button and uploaded_file is not None and mcq_count and grade and tone:
                     {
                         "text": text,
                         "number": mcq_count,
-                        "grade": grade,
+                        "level": level,
                         "tone": tone,
                         "response_json": json.dumps(RESPONSE_JSON),
                     }
